ff3046d5d47381c9b6565ae47942e6bbbd0edf5d
hiaximize <dbarkerit@gmail.com>
2019-09-14 00:47:08 -0400

    added a gif to the header, added project section for gifs

--- src/Skillz.js
+++ src/Skillz.js
@@ -7,57 +7,57 @@ class Skillz extends React.Component{
 
                     <div className="skill swirl-in-fwd" id="node">
                         {/* <h3> Node.js</h3> */}
-                        <img className="skillPicture swirl-in-fwd" src="node.png" id="node"></img>
+                        <img className="skillPicture swirl-in-fwd" src="node.png" id="node" alt="Node.js"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd">
                         {/* <h3>Javascript</h3> */}
-                        <img id="javascript" className="skillPicture" src="javascript.png"></img>
+                        <img id="javascript" className="skillPicture" src="javascript.png" alt="Javascript"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="angular">
                         {/* <h3>Angular</h3> */}
-                        <img className="skillPicture" src="angular.png"></img>
+                        <img className="skillPicture" src="angular.png" alt="Angular"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="html">
                         {/* <h3>HTML</h3> */}
-                        <img className="skillPicture" src="html.png"></img>
+                        <img className="skillPicture" src="html.png" alt="HTML"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="css">
                         {/* <h3>CSS</h3> */}
-                        <img className="skillPicture" src="css.png"></img>
+                        <img className="skillPicture" src="css.png" alt="CSS"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="mongo">
                         {/* <h3>Mongo</h3> */}
-                        <img className="skillPicture" src="mongodb.png"></img>
+                        <img className="skillPicture" src="mongodb.png" alt="Mongo"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="python">
                         {/* <h3>Python</h3> */}
-                        <img className="skillPicture" src="python.png"></img>
+                        <img className="skillPicture" src="python.png" alt="Python"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="ruby">
                         {/* <h3>Ruby</h3> */}
-                        <img className="skillPicture" src="ruby.png"></img>
+                        <img className="skillPicture" src="ruby.png" alt="Ruby"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="relationDb">
                         {/* <h3>PostgreSQL</h3> */}
-                        <img className="skillPicture" src="sql.png"></img>
+                        <img className="skillPicture" src="sql.png" alt="PostGreSQL"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="JQuery">
                         {/* <h3>JQuery</h3> */}
-                        <img className="skillPicture" src="jquery.png"></img>
+                        <img className="skillPicture" src="jquery.png" alt="JQuery"></img>
                     </div>
 
                     <div className="skill swirl-in-fwd" id="react">
                         {/* <h3>React</h3> */}
-                        <img className="skillPicture" src="#"></img>
+                        <img className="skillPicture" src="#" alt="React"></img>
                     </div>
             </div>  
         )
