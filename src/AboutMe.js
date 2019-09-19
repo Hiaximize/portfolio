05325ec9afe00ec8cff5c3cbfe774b5dcc052380
hiaximize <dbarkerit@gmail.com>
2019-09-19 12:40:24 -0400

    fixed link for membership project and put alt saying its coming soon. Added tenacious developer icon to the about me page.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,17 +1,14 @@
 import React from 'react'
 
 class AboutMe extends React.Component{
-    constructor(props){
-        super(props)
 
-    }
     render(){
         return(
             <div id="aboutMeContainer">
 
                 <div id="aboutMeContent">
                     <div id="avatar">
-                        <img className="slide-in-right" id="derek" src="Derek.png"></img>
+                        <img alt="Derek's Gravatar" className="slide-in-right" id="derek" src="Derek.png"></img>
                     </div>    
                     <div className="slide-in-left" id="welcomeMessage">
                         <h6>My name is Derek Barker. I'm a US Army combat Vet, and transitioned to Full-Stack Developer. Solving complex challenges using simple but elegant solutions for clients is something that drives me. </h6>         
@@ -21,13 +18,13 @@ class AboutMe extends React.Component{
                 
 
                 <div id="aboutMeTileContainer">
-                    <div className="aboutMeTile scale-in-center"><img className="aboutMeImg scale-in-center" src="userfocused.png"/> User Focused</div>
+                    <div className="aboutMeTile scale-in-center"><img className="aboutMeImg scale-in-center" alt="UserFocusedImage" src="userfocused.png"/> User Focused</div>
 
-                    <div className="aboutMeTile scale-in-center"><img className="aboutMeImg scale-in-center" src="responsiveImage.png"/>Responsive Designs</div>
+                    <div className="aboutMeTile scale-in-center"><img alt="Responsive Web Design" className="aboutMeImg scale-in-center" src="responsiveImage.png"/>Responsive Designs</div>
 
-                    <div className="aboutMeTile scale-in-center"><img className="aboutMeImg scale-in-center" src="frontBackEnd.png" />Front & Back End</div>
+                    <div className="aboutMeTile scale-in-center"><img alt="Front and Backend Development" className="aboutMeImg scale-in-center" src="frontBackEnd.png" />Front & Back End</div>
 
-                    <div className="aboutMeTile scale-in-center">Tenacious Developer</div>
+                    <div className="aboutMeTile scale-in-center"><img src="tenaciousDeveloper.png" alt="Tenacious Developer" className="aboutMeImg scale-in-center" /> Tenacious Developer</div>
                 </div>
 
             </div>
