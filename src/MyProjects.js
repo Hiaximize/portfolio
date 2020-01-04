01ba3befd1f6afa73a9d85c89c6197b25fcfd1ee
hiaximize <dbarkerit@gmail.com>
2020-01-04 00:12:30 -0500

    added new projects

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -7,6 +7,10 @@ class MyProjects extends React.Component{
                 
                 <div id="projectsContainer">
                     
+                    <a className="projects" rel="noopener noreferrer" target="_blank" href="http://www.apexelitedirectmarketing.com"><img className="slit-in-vertical projectsImg" id="apex" src="/Dereks_Portfolio_files/apexDirectMarketing.png" alt="Apex Elite Direct Marketing"></img></a>
+
+                    <a className="projects" rel="noopener noreferrer" target="_blank" href="http://www.incrediblecutz.herokuapp.com"><img className="slit-in-vertical projectsImg" id="incredibleCutz" src="/Dereks_Portfolio/incredibleCutz.png" alt="Incredible Cutz Barbershop"></img></a>
+
                     <a className="projects" rel="noopener noreferrer" target="_blank" href="https://hiaximize.github.io"><img className="slit-in-vertical projectsImg" id="rickAndMorty" src="rickAndMorty.gif" alt="RickyAndMorty"></img></a>
 
                     {/* <hr align="center" className="projectsHr"/> */}
@@ -23,7 +27,6 @@ class MyProjects extends React.Component{
 
                     {/* <hr className="projectsHr"/> */}
 
-                    <a className="projects" rel="noopener noreferrer" target="_blank" href="https://rocky-waters-58456.herokuapp.com"><img className="slit-in-vertical projectsImg" id="finalProject" src="#" alt="MembershipTracker Coming Soon"></img></a>
                 </div>
             </div>
         )
